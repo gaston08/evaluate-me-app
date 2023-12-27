96176041149d7e0ec2d908133549c92f4dff0d0c
gaston pedraza <gaston08pedraza@gmail.com>
2023-12-27 15:43:10 -0300

    add blog layout

---
+++ src/app/layouts/blog/components/Main/Main.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import { Outlet } from 'react-router-dom';
+
+export default function Main() {
+  return (
+    <Grid
+      item
+      xs={12}
+      md={8}
+      sx={{
+        '& .markdown': {
+          py: 3,
+        },
+      }}
+    >
+      <Outlet />
+    </Grid>
+  );
+}
--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,6 +3,7 @@ import Error from './pages/error';
 import Main from './pages/main';
 
 import MainLayout from './layouts/main';
+import BlogLayout from './layouts/blog';
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +14,16 @@ export const router = createBrowserRouter([
       {
         path: '/main/index',
         element: <h1>Hola mundo</h1>,
+      },
+    ],
+  },
+  {
+    path: '/blog',
+    element: <BlogLayout />,
+    children: [
+      {
+        path: '/blog/1',
+        element: <h1>Este es el blog 1</h1>,
       },
     ],
   },
