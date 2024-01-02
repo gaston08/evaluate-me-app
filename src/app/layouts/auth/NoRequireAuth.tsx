4f013ab788a9ca5e63787ec71104553a3814627a
gaston pedraza <gaston08pedraza@gmail.com>
2024-01-02 13:25:12 -0300

    add auth protect routes

---
+++ src/app/layouts/auth/NoRequireAuth.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { axiosPost } from '../../utils/axios';
+import axios from 'axios';
+
+export default function RequireAuth() {
+	const [isLoading, setIsLoading] = useState(true);
+	const navigate = useNavigate();
+
+	const checkAuth = async () => {
+		const access_token = localStorage.getItem('access_token');
+		if (access_token) {
+			axios.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
+			const result = await axiosPost('auth', {});
+			if (result.ok) {
+				navigate('/blog/exam');
+			} else {
+				setIsLoading(false);
+			}
+		} else {
+			setIsLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		checkAuth();
+	}, []);
+
+	return <>{isLoading ? <h1>Loading...</h1> : <Outlet />}</>;
+}
