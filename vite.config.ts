c03e6ff47b6cf93dcc3f07adc10d4147d934e768
gaston pedraza <gaston08pedraza@gmail.com>
2024-01-02 22:15:49 -0300

    basic config for jest

--- vite.config.ts
+++ vite.config.ts
@@ -32,18 +32,5 @@ export default defineConfig({
     },
   },
 
-  test: {
-    globals: true,
-    cache: {
-      dir: './node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './coverage/evaluate-me-app',
-      provider: 'v8',
-    },
-  },
+  test: {},
 });
