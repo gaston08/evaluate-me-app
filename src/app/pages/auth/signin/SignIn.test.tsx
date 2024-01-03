0d1d57a7e80d9ad200d4742a717e95d8b0945edf
gaston pedraza <gaston08pedraza@gmail.com>
2024-01-02 22:25:14 -0300

    simple test

---
+++ src/app/pages/auth/signin/SignIn.test.tsx
@@ -0,0 +1,14 @@
+import { it, expect } from '@jest/globals';
+import { fireEvent, render } from '@testing-library/react';
+//import SignIn from './SignIn';
+
+it('SignIn render', () => {
+  //const { queryByLabelText, getByLabelText } = render(<SignIn />);
+
+  //expect(queryByLabelText(/Inicio de sesión/)).toBeTruthy();
+
+  //fireEvent.click(getByLabelText(/off/i));
+
+  //expect(queryByLabelText(/on/i)).toBeTruthy();
+  expect(true).toBeTruthy();
+});
