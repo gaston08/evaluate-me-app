2559f7eb973e308cf22704d9ad453a6b5ef62367
gaston pedraza <gaston08pedraza@gmail.com>
2024-01-06 17:16:38 -0300

    test

--- src/app/components/ThemeWrapper/ThemeWrapper.tsx
+++ src/app/components/ThemeWrapper/ThemeWrapper.tsx
@@ -1,5 +1,6 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import React from 'react';
 
 declare module '@mui/material/styles' {
 	interface Theme {
