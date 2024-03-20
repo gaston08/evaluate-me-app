2f57587a1ab529d9569303283ed057c1428c8bca
gaston pedraza <gaston08pedraza@gmail.com>
2024-03-20 13:57:52 -0300

    add exam simple view

--- src/app/pages/exam/view/View.tsx
+++ src/app/pages/exam/view/View.tsx
@@ -1,37 +1,68 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import { apiGetResponse } from 'app/shared/interfaces/api-response';
 import { axiosGet } from 'app/utils/axios';
+import NoExamFound from './NoExamFound';
+import Exercises from '../components/Exercises';
+import { ExamContext } from 'app/contexts/Exam';
+import { createExam } from 'app/shared/interfaces/exam';
 
 export default function Exam() {
 	const params = useParams();
+	const [loading, setLoading] = useState<boolean>(false);
+	const [errors, setErrors] = useState<Array<string>>([]);
+	const { exam, setExam } = useContext<createExam>(ExamContext);
 
 	useEffect(() => {
 		async function fetchData() {
+			setLoading(true);
 			const result: apiGetResponse = await axiosGet(
 				`api/exam/find:${params.id}`,
 			);
 			console.log(result);
 			if (result.ok) {
+				setExam(result.data.exam);
+				setLoading(false);
 			} else {
-				/*setError(result.error);
-					setOpen(true);
-					if (result.errors) {
-						result.errors.forEach((err: expressError): void => {
-							setError(err.msg);
-							setOpen(true);
-						});
-					}
-					setLoading(false);*/
+				if (result.errors) {
+					const errorsArr: Array<string> = result.errors.map((err) => {
+						return err.msg;
+					});
+
+					setErrors(errorsArr);
+				}
+				setLoading(false);
 			}
 		}
 
-		try {
-			fetchData().then().catch(console.error);
-		} catch (err) {
-			console.log(err);
-		}
+		fetchData().then().catch(console.error);
 	}, []);
 
-	return <>Hola</>;
+	return (
+		<>
+			{loading ? (
+				<Typography variant="h3">Cargando...</Typography>
+			) : (
+				<>
+					{errors.length !== 0 ? (
+						<NoExamFound errors={errors} />
+					) : (
+						<>
+							<Box sx={{ mb: 5 }}>
+								<Typography variant="h5" sx={{ mb: 2 }}>
+									{exam.category}, {exam.year}
+								</Typography>
+								<Typography variant="h5">
+									{exam.type}, TEMA {exam.exam_number}
+								</Typography>
+							</Box>
+							<Exercises />
+						</>
+					)}
+				</>
+			)}
+		</>
+	);
 }
