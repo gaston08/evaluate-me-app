796fca9681d838511430f852436a7d135cf60054
gaston08 <gaston08pedraza@gmail.com>
2024-09-02 22:09:40 -0300

    add payment method

--- src/app/pages/store/Store/components/Prices/components/PaymentMethods/Payment.tsx
+++ src/app/pages/store/Store/components/Prices/components/PaymentMethods/Payment.tsx
@@ -1,13 +1,67 @@
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { CoffeeIcon } from '../CoffeePrices/CoffeePrices';
 
 export default function Payment() {
 	return (
 		<Box>
 			<Typography>
-				{' '}
-				<strong>Pagá con mercado pago</strong>{' '}
+				<strong>Pagá con mercado pago</strong>
 			</Typography>
+			<Box sx={{ pt: 2, pb: 2 }}>
+				<Typography>
+					<strong>Pasos a seguir.</strong>
+				</Typography>
+				<Box sx={{ mt: 1 }}>
+					<ol>
+						<li>
+							<Typography>Pagá el pack que deseas adquirir.</Typography>
+							<Box sx={{ mt: 2 }}>
+								<PackListItem coffees="100" />
+								<PackListItem coffees="1.000" />
+								<PackListItem coffees="10.000" />
+							</Box>
+						</li>
+						<li>
+							<Typography>
+								Una vez realizado el pago, recarga la tienda para ver los
+								cambios en tus cafecitos.
+							</Typography>
+						</li>
+					</ol>
+				</Box>
+			</Box>
+		</Box>
+	);
+}
+
+interface PackListItemProps {
+	coffees: string;
+}
+
+function PackListItem(props: PackListItemProps) {
+	const { coffees } = props;
+
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'space-around',
+				gap: 1,
+				mb: 3,
+			}}
+		>
+			<Box sx={{ display: 'flex', alignItems: 'center' }}>
+				<CoffeeIcon />
+				<Typography sx={{ ml: 1 }}>
+					<strong>x{coffees}</strong>
+				</Typography>
+			</Box>
+			<Button variant="contained" size="small" color="success">
+				PAGAR
+			</Button>
 		</Box>
 	);
 }
