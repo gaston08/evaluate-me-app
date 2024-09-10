2f66e77d818014b0ab936250ff424a7f9ee31f1f
gaston08 <gaston08pedraza@gmail.com>
2024-09-10 17:21:41 -0300

    remove confetti

--- src/app/pages/exam/View/components/ExamResult/ExamResult.tsx
+++ src/app/pages/exam/View/components/ExamResult/ExamResult.tsx
@@ -2,7 +2,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import ReactTimeAgo from 'react-time-ago';
 import { useTheme } from '@mui/material/styles';
-import Confetti from 'app/components/Confetti';
+//import Confetti from 'app/components/Confetti';
 
 interface ExamResultProps {
 	score: number;
@@ -20,31 +20,31 @@ export default function ExamResult(props: ExamResultProps) {
 	function getDataRender() {
 		let text: string;
 		let color: string;
-		let confetties: number;
+		//let confetties: number;
 		const roundedScore = Math.ceil((score / totalPts) * 10);
 		switch (roundedScore) {
 			case 10:
 				text = '¡Perfecto!';
 				color = theme.palette.success.light;
-				confetties = 500;
+				//confetties = 500;
 				break;
 			case 9:
 				text = '¡Impresionante!';
 				color = theme.palette.success.light;
-				confetties = 300;
+				//confetties = 300;
 				break;
 			case 7:
 			case 8:
 				text = '¡Excelente!';
 				color = theme.palette.success.main;
-				confetties = 100;
+				//confetties = 100;
 				break;
 			case 6:
 			case 5:
 			case 4:
 				text = '¡Muy bien!';
 				color = theme.palette.success.main;
-				confetties = 0;
+				//confetties = 0;
 				break;
 			case 3:
 			case 2:
@@ -52,7 +52,7 @@ export default function ExamResult(props: ExamResultProps) {
 			case 0:
 				text = 'Ups... Sigue intentando!';
 				color = theme.palette.error.main;
-				confetties = 0;
+				//confetties = 0;
 				break;
 		}
 
@@ -61,10 +61,10 @@ export default function ExamResult(props: ExamResultProps) {
 			audio.play().catch(console.error);
 		}
 
-		return { text, color, confetties };
+		return { text, color };
 	}
 
-	const { text, color, confetties } = getDataRender();
+	const { text, color } = getDataRender();
 
 	return (
 		<Box sx={{ mb: 3 }} id="exam-result">
@@ -78,7 +78,7 @@ export default function ExamResult(props: ExamResultProps) {
 				<ReactTimeAgo date={new Date(date)} locale="es-AR" />
 			</Typography>
 			<Box sx={{ position: 'fixed', top: 0 }}>
-				<Confetti confetties={confetties} />
+				{/*<Confetti confetties={confetties} />*/}
 			</Box>
 		</Box>
 	);
