60929ebd0c5eee90355afad4035fde2c53701e3c
gaston08 <gaston08pedraza@gmail.com>
2024-10-02 16:05:09 -0300

    remove subject exams

--- src/app/pages/exam/index.tsx
+++ src/app/pages/exam/index.tsx
@@ -1,5 +1,4 @@
 export { default as View } from './View';
 export { default as Subjects } from './Subjects';
-export { default as SubjectExams } from './SubjectExams';
 export { default as Trainer } from './Trainer';
 export { default as TrainerLobby } from './TrainerLobby';
