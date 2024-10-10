60b1859f549de81e8fdd516e75c802b6b442058a
gaston08 <gaston08pedraza@gmail.com>
2024-10-10 13:15:03 -0300

    clean

--- src/app/pages/exam/components/SeoWrappers/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as IpcSeoWrapper } from './Ipc';
-export { default as IcseSeoWrapper } from './Icse';
-export { default as DDHHSeoWrapper } from './DDHH';
-export { default as DerechoPrivado } from './DDHH';
-export { default as TrabajoYSociedad } from './TrabajoYSociedad';
-export { default as Antropologia } from './Antropologia';
-export { default as Sociologia } from './Sociologia';
-export { default as PensamientoComputacional } from './PensamientoComputacional';
